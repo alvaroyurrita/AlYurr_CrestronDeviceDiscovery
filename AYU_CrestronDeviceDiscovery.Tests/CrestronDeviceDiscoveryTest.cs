e4628cb740ab458464625a843dc4f880eb4c6e7e
Alvaro Yurrita <ayurrita@dgicommunications.com>
2024-07-02 17:13:12 -0400

    Initial Commit

---
+++ AYU_CrestronDeviceDiscovery.Tests/CrestronDeviceDiscoveryTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using AYU_CrestronDeviceDiscovery;
+using NUnit.Framework;
+using Serilog;
+using Serilog.Core;
+
+namespace AYU_CrestronDeviceDiscovery.Tests;
+[TestFixture]
+[TestOf(typeof(CrestronDeviceDiscovery))]
+public class CrestronDeviceDiscoveryTest
+{
+    private readonly Logger _log = new LoggerConfiguration().WriteTo.NUnitOutput().CreateLogger();
+    [Test]
+    public async Task DiscoverLocal_ShouldShowSomeDevices()
+    {
+        var devicesDiscovered = new List<CrestronDeviceEventArgs>();
+        CrestronDeviceDiscovery.DeviceDiscovered += (sender, args) =>
+        {
+            devicesDiscovered.Add(args);
+            _log.Information("Device Discovered: {Name}",args.Hostname);
+        };
+        var devices = await CrestronDeviceDiscovery.DiscoveryLocal();
+        Assert.That(devices.Count, Is.EqualTo(devicesDiscovered.Count));
+    }
+}
---
+++ AYU_CrestronDeviceDiscovery/CrestronDeviceEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace AYU_CrestronDeviceDiscovery;
+public class CrestronDeviceEventArgs : EventArgs
+{
+    public string IpAddress { get; set; } = "";
+    public string Hostname { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string DeviceId { get; set; } = "";
+}
